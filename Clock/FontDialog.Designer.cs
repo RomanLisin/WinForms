6e4686298eb9db62b81691cd1a3d6f88e5ea988f
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-01-15 23:49:53 +1000

    LoadFont Done

--- Clock/FontDialog.Designer.cs
+++ Clock/FontDialog.Designer.cs
@@ -32,22 +32,23 @@
 			this.comboBoxFonts = new System.Windows.Forms.ComboBox();
 			this.labelChooseFont = new System.Windows.Forms.Label();
 			this.labelFontSize = new System.Windows.Forms.Label();
-			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDownFontSize = new System.Windows.Forms.NumericUpDown();
 			this.labelExample = new System.Windows.Forms.Label();
 			this.buttonOk = new System.Windows.Forms.Button();
 			this.buttonCancel = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDownFontSize)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// comboBoxFonts
 			// 
 			this.comboBoxFonts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBoxFonts.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.comboBoxFonts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.comboBoxFonts.FormattingEnabled = true;
 			this.comboBoxFonts.Location = new System.Drawing.Point(25, 52);
 			this.comboBoxFonts.Name = "comboBoxFonts";
-			this.comboBoxFonts.Size = new System.Drawing.Size(334, 32);
+			this.comboBoxFonts.Size = new System.Drawing.Size(334, 28);
 			this.comboBoxFonts.TabIndex = 0;
+			this.comboBoxFonts.SelectedIndexChanged += new System.EventHandler(this.comboBoxFonts_SelectedIndexChanged);
 			// 
 			// labelChooseFont
 			// 
@@ -69,14 +70,29 @@
 			this.labelFontSize.TabIndex = 2;
 			this.labelFontSize.Text = "Размер шрифта";
 			// 
-			// numericUpDown1
+			// numericUpDownFontSize
 			// 
-			this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.numericUpDown1.Location = new System.Drawing.Point(416, 55);
-			this.numericUpDown1.Name = "numericUpDown1";
-			this.numericUpDown1.Size = new System.Drawing.Size(105, 29);
-			this.numericUpDown1.TabIndex = 3;
-			this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.numericUpDownFontSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.numericUpDownFontSize.Location = new System.Drawing.Point(416, 55);
+			this.numericUpDownFontSize.Maximum = new decimal(new int[] {
+            58,
+            0,
+            0,
+            0});
+			this.numericUpDownFontSize.Minimum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+			this.numericUpDownFontSize.Name = "numericUpDownFontSize";
+			this.numericUpDownFontSize.Size = new System.Drawing.Size(105, 26);
+			this.numericUpDownFontSize.TabIndex = 3;
+			this.numericUpDownFontSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.numericUpDownFontSize.Value = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
 			// 
 			// labelExample
 			// 
@@ -115,14 +131,14 @@
 			this.Controls.Add(this.buttonCancel);
 			this.Controls.Add(this.buttonOk);
 			this.Controls.Add(this.labelExample);
-			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.numericUpDownFontSize);
 			this.Controls.Add(this.labelFontSize);
 			this.Controls.Add(this.labelChooseFont);
 			this.Controls.Add(this.comboBoxFonts);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "FontDialog";
 			this.Text = "Choose clock font";
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDownFontSize)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -133,7 +149,7 @@
 		private System.Windows.Forms.ComboBox comboBoxFonts;
 		private System.Windows.Forms.Label labelChooseFont;
 		private System.Windows.Forms.Label labelFontSize;
-		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.NumericUpDown numericUpDownFontSize;
 		private System.Windows.Forms.Label labelExample;
 		private System.Windows.Forms.Button buttonOk;
 		private System.Windows.Forms.Button buttonCancel;
--- Clock/FontDialog.cs
+++ Clock/FontDialog.cs
@@ -8,23 +8,48 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Drawing.Text;
+using System.Linq.Expressions;  // for PrivateFontCollection
 
 namespace Clock
 {
 	public partial class FontDialog : Form
 	{
+		string execution_path = ""; 
+			string fonts_path = "";
 		public FontDialog()
 		{
 			InitializeComponent();
+			execution_path = Path.GetDirectoryName(Application.ExecutablePath);
+			fonts_path = $"{execution_path}\\..\\..\\Fonts";
 			LoadFonts();
 		}
 		void LoadFonts()
 		{
-			string execution_path = Path.GetDirectoryName(Application.ExecutablePath);
             Console.WriteLine(execution_path);
-			string fonts_path = $"{execution_path}\\..\\..\\Fonts";
-			Directory.SetCurrentDirectory(fonts_path);
-            Console.WriteLine(Directory.GetCurrentDirectory());
+			//Directory.SetCurrentDirectory(fonts_path);
+   //         Console.WriteLine(Directory.GetCurrentDirectory());
+
+			comboBoxFonts.Items.AddRange(GetFontsFromDirectory(fonts_path,"*.ttf"));
+			comboBoxFonts.Items.AddRange(GetFontsFromDirectory(fonts_path,"*.otf"));
+			comboBoxFonts.SelectedIndex = 0; // для выбора первого шрифта
         }
+		string[] GetFontsFromDirectory(string directory, string format)
+		{
+			string[] fonts = Directory.GetFiles(directory, format);
+			for (int i = 0; i<fonts.Length;i++)
+			{
+				fonts[i] = fonts[i].Split('\\').Last();
+			}
+			return fonts;
+
+		}
+
+		private void comboBoxFonts_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			PrivateFontCollection pfc = new PrivateFontCollection();
+			pfc.AddFontFile($"{fonts_path}\\{comboBoxFonts.SelectedItem}");
+			labelExample.Font = new Font(pfc.Families[0], Convert.ToInt32(numericUpDownFontSize.Value));
+		}
 	}
 }
